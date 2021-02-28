6c27050b7d919fec3508e9074b0944b2985f0589
Mahesh <53971467+maheshwaranvk@users.noreply.github.com>
2021-02-28 19:18:28 +0530

    class

---
+++ src/main/java/staticVariables/ClassA.java
@@ -0,0 +1,8 @@
+package staticVariables;
+
+public class ClassA {
+
+	static String name ="mahesh";
+	
+	
+}
---
+++ src/main/java/staticVariables/ClassB.java
@@ -0,0 +1,9 @@
+package staticVariables;
+
+public class ClassB extends ClassA{
+
+	public static void main(String[] args) {
+		name = name.concat("waran");
+		System.out.println(name);
+	}
+}
